2a8ecd5af5f8b5e841e65e7f2137e2a891baef08
mihrpsah <smihirp1@gmail.com>
2022-10-29 19:14:54 +0530

    Formatted Code

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import Amazon from "../images/logo.png";
 import BookStore from "../images/bookstore.png";
 import USA from "../images/usa.png";
 //import React from 'react';
-import {ShoppingCartOutlined, MenuOutlined} from "@ant-design/icons";
+import { ShoppingCartOutlined, MenuOutlined } from "@ant-design/icons";
 import { Link } from 'react-router-dom';
 
 
@@ -26,31 +26,31 @@ const onSearch = (value) => console.log(value);
 
 const Header = () => {
   const { authenticate } = useMoralis();
-  return(
+  return (
     <div className="site-page-header-ghost-wrapper">
       <PageHeader
         ghost={false}
-        
+
         extra={[
 
           <>
-          <img src={Amazon} className="logo"></img>
-          <img src={BookStore} className="logo"></img>
+            <img src={Amazon} className="logo"></img>
+            <img src={BookStore} className="logo"></img>
 
-          <Search
-      placeholder="Search For Products"
-      enterButton="Search"
-      size="large"
-      suffix={suffix}
-      onSearch={onSearch}
-      className="searchBar"
-    />
-          
-         <Button key="1" type="primary" class="login" onClick={() => authenticate()}>
-          Login
-          </Button>
-          <Space size={"large"}>
-              
+            <Search
+              placeholder="Search For Products"
+              enterButton="Search"
+              size="large"
+              suffix={suffix}
+              onSearch={onSearch}
+              className="searchBar"
+            />
+
+            <Button key="1" type="primary" class="login" onClick={() => authenticate()}>
+              Login
+            </Button>
+            <Space size={"large"}>
+
               <Badge count={0} showZero>
                 <span className="header-buttons">
                   <ShoppingCartOutlined className="header-icon" />
@@ -60,7 +60,7 @@ const Header = () => {
               <Space className="header-buttons" size={"small"}>
                 <img src={USA} alt="region" className="flag"></img>▾
               </Space>
-              
+
             </Space>
 
           </>
@@ -68,21 +68,21 @@ const Header = () => {
       </PageHeader>
 
       <div className="site-page-subheader-ghost-wrapper">
-      <Space size={"middle"}>
-        <Space size={"small"} style={{fontWeight:"bold"}}>
-          <MenuOutlined />
-          Categories
-        </Space>
-        {categories.map((e) =>{
-          return(
-            <Link to="/categories" state={e} className="categories">
-              {e}
-            </Link>
-          )
+        <Space size={"middle"}>
+          <Space size={"small"} style={{ fontWeight: "bold" }}>
+            <MenuOutlined />
+            Categories
+          </Space>
+          {categories.map((e) => {
+            return (
+              <Link to="/categories" state={e} className="categories">
+                {e}
+              </Link>
+            )
 
-        })}
-      </Space>
-    </div>
+          })}
+        </Space>
+      </div>
     </div>
   )
 }
