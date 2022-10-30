93ee272644198d058ae9a79149548d5ed94e2225
mihrpsah <smihirp1@gmail.com>
2022-10-30 14:07:13 +0530

    Added functionality

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Button } from 'antd';
+import { useLocation } from 'react-router-dom';
 
 const Categories = () => {
-
+const {state: category} = useLocation();
 return(
   <>
   <div className="container">
-    <Button type="primary">Categories Page</Button>
+    <Button type="primary"
+    onClick={() => console.log(category)}>Categories Page</Button>
   </div>
   </>
 )
